8a61ae8c4716a4645f1e95482d0c9fc74b8b30cf
Peter-Teng <aaa70256@gmail.com>
2023-02-27 14:07:58 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
 		SOURCETREE tEST 1 develop commit 66666666
-		oooooooooooooooooooooooooooooooooooooooo
+		oooooooooooooooooooooooooooooooooooooooo master
 	</div>
 </template>
 
